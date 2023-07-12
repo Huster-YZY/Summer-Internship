9a470efb60051970fc4c48c439f7706eccc7cd4b
zhenya <2928664165@qq.com>
2023-07-12 20:55:38 +0800

    still have some problems

--- lr_ti/hw1/exp_ms.py
+++ lr_ti/hw1/exp_ms.py
@@ -4,23 +4,21 @@ ti.init(arch=ti.cpu)
 
 # the scale of the simulation
 n = 128
+use_implicit = False
+iter_number = 3
 quad_size = 1.0 / n
 
-dt = 3.125e-4
-# dt = 4e-2 / n
+# dt = 3.125e-4
+dt = 4e-4 / n
 
 # substeps=53,the number of iteration if we want to achieve 60fps
-substeps = int(1 / 10 // dt)
+substeps = int(1 / 60 // dt)
 
 gravity = ti.Vector([0, -9.8, 0])
 I = ti.Matrix([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
-spring_Y = 3e4
+spring_Y = 3
 dashpot_damping = 1
 drag_damping = 1
-# assume each particle's mass is Identity matrix
-# M = ti.Matrix.field(3, 3, float, (n, n))
-
-J = ti.Matrix.field(3, 3, float, shape=(n, n))
 
 # balls' model
 ball_radius = 0.3
@@ -33,13 +31,21 @@ x = ti.Vector.field(3, dtype=float, shape=(n, n))
 v = ti.Vector.field(3, dtype=float, shape=(n, n))
 
 num_triangles = (n - 1) * (n - 1) * 2
+num_node = int(n * n)
 indices = ti.field(int, shape=num_triangles * 3)
 vertices = ti.Vector.field(3, dtype=float, shape=n * n)
 colors = ti.Vector.field(3, dtype=float, shape=n * n)
 
+#Implicit method
+F = ti.Vector.field(3, dtype=ti.f32, shape=num_node)
+J = ti.Matrix.field(3, 3, dtype=ti.f32, shape=(num_node, num_node))
+A = ti.Matrix.field(3, 3, dtype=ti.f32, shape=(num_node, num_node))
+M = ti.field(dtype=ti.f32, shape=(num_node, num_node))
+new_v = ti.Vector.field(3, dtype=ti.f32, shape=num_node)
+b = ti.Vector.field(3, dtype=ti.f32, shape=num_node)
+
 # configuration
 bending_springs = False
-use_implicit = True
 
 
 @ti.kernel
@@ -87,29 +93,7 @@ else:
                 spring_offsets.append(ti.Vector([i, j]))
 
 
-@ti.func
-def clr(m):
-    for i in range(3):
-        for j in range(3):
-            m[i, j] = 0.0
-
-
-@ti.func
-def Jacobi_iterations(A, idx, b):
-    new_x = ti.Vector([0.0, 0.0, 0.0])
-
-    for i in range(3):
-        r = b[i]
-        for j in range(3):
-            if i != j:
-                r -= A[i, j] * v[idx][j]
-        new_x[i] = r / A[i, i]
-
-    for i in range(3):
-        v[idx][i] = new_x[i]
-
-
-@ti.func
+@ti.kernel
 def collision_detection():
     for i in ti.grouped(x):
         v[i] *= ti.exp(-drag_damping * dt)
@@ -121,40 +105,103 @@ def collision_detection():
         x[i] += v[i] * dt
 
 
-# Most Important Function
+@ti.func
+def m2v(x):
+    return x[0] * n + x[1]
+
+
+@ti.func
+def v2m(x: int):
+    return (int(x / num_node), int(x % num_node))
 
 
 @ti.kernel
-def exp_substep():
-    for i in ti.grouped(x):
-        v[i] += gravity * dt
+def init_m():
+    for i in range(num_node):
+        M[i, i] = 1.0
+
+
+@ti.kernel
+def update_force():
 
     for i in ti.grouped(x):
-        force = ti.Vector([0.0, 0.0, 0.0])
+        idx = m2v(i)
         for spring_offset in ti.static(spring_offsets):
             j = i + spring_offset
             if 0 <= j[0] < n and 0 <= j[1] < n:
                 x_ij = x[i] - x[j]
-                v_ij = v[i] - v[j]
                 d = x_ij.normalized()
                 current_dist = x_ij.norm()
                 original_dist = quad_size * float(i - j).norm()
+                F[idx] += -spring_Y * d * (current_dist / original_dist - 1)
 
-                force += -spring_Y * d * (current_dist / original_dist - 1)
-                force += -v_ij.dot(d) * d * dashpot_damping * quad_size
 
-                # implicit
-                mat = d.outer_product(d)
-                J[i] += -(spring_Y / original_dist) * (
-                    (1 - original_dist / current_dist) * (I - mat) + mat)
+@ti.kernel
+def update_Jacobi():
+    for i, d in J:
+        J[i, d] *= 0.0
+        idx_i = v2m(i)
+        for offset in ti.static(spring_offsets):
+            idx_j = idx_i + offset
+            j = m2v(idx_j)
+            if d == i or d == j:
+                x_ij = x[idx_i] - x[idx_j]
+                dir = x_ij.normalized()
+                mat = dir.outer_product(dir)
+                r = quad_size * float(idx_i - idx_j).norm()
+                l = x_ij.norm()
+                jx = -(spring_Y / r) * ((1 - r / l) * (I - mat) + mat)
+                if d == i:
+                    J[i, d] += jx
+                else:
+                    J[i, d] -= jx
 
-        # v[i] += force * dt
 
-        # implicit
-        A = I - J[i] * dt**2
-        b = J[i] @ v[i] * dt * dt + dt * force
-        v[i] += A.inverse() @ b
+@ti.kernel
+def update_A():
+    for i, j in A:
+        A[i, j] = M[i, j] * I - dt * dt * J[i, j]
 
+
+@ti.kernel
+def update_b():
+    for i in b:
+        b[i] = v[v2m(i)] + dt * F[i]
+
+
+@ti.kernel
+def Jacobi_iteration():
+    n = num_node
+    for i in range(n):
+        r = b[i]
+        for j in range(n):
+            if i != j:
+                r -= A[i, j] @ v[v2m(j)]
+        new_v[i] = A[i, i].inverse() @ r
+    for i in ti.grouped(v):
+        v[i] = new_v[m2v(i)]
+
+
+def implicit_substep(iter_number):
+    init_m()
+    update_Jacobi()
+    update_A()
+    update_force()
+    update_b()
+    for _ in range(iter_number):
+        Jacobi_iteration()
+    collision_detection()
+
+
+@ti.kernel
+def update_velocity():
+    for i in ti.grouped(v):
+        v[i] += F[m2v(i)] * dt
+
+
+def explicit_substep():
+    update_force()
+    update_velocity()
     collision_detection()
 
 
@@ -186,7 +233,10 @@ while window.running:
         current_t = 0
 
     for i in range(substeps):
-        exp_substep()
+        if use_implicit:
+            implicit_substep(iter_number)
+        else:
+            explicit_substep()
         current_t += dt
     update_vertices()
 
