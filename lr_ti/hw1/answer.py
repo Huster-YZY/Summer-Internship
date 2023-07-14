2a62afc6408119a7ce23b08ac45af5581c2751db
zhenya <2928664165@qq.com>
2023-07-14 17:01:45 +0800

    implicit mass-spring solver is done

--- lr_ti/hw1/answer.py
+++ lr_ti/hw1/answer.py
@@ -1,57 +1,56 @@
 import taichi as ti
 
-ti.init(arch=ti.cpu)
-
-# the scale of the simulation
-n = 10
+ti.init(arch=ti.vulkan)  # Alternatively, ti.init(arch=ti.cpu)
 use_implicit = True
-iter_number = 10
+
+n = 50
+num_iteration = 3
 quad_size = 1.0 / n
-
-# dt = 3.125e-3
-dt = 4e-2 / n
-
-# substeps=53,the number of iteration if we want to achieve 60fps
+dt = 8e-2 / n
 substeps = int(1 / 60 // dt)
 
 gravity = ti.Vector([0, -9.8, 0])
-I = ti.Matrix([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
-spring_Y = 3e4
-dashpot_damping = 1
+spring_Y = 3e2
+dashpot_damping = 1e4
 drag_damping = 1
 
-# balls' model
 ball_radius = 0.3
 ball_center = ti.Vector.field(3, dtype=float, shape=(1, ))
 ball_center[0] = [0, 0, 0]
-# ball_center[1] = [0.4, 0, 0]
-# ball_center[2] = [-0.4, 0, 0]
 
 x = ti.Vector.field(3, dtype=float, shape=(n, n))
 v = ti.Vector.field(3, dtype=float, shape=(n, n))
 
 num_triangles = (n - 1) * (n - 1) * 2
-num_node = int(n * n)
+num_nodes = n * n
 indices = ti.field(int, shape=num_triangles * 3)
 vertices = ti.Vector.field(3, dtype=float, shape=n * n)
 colors = ti.Vector.field(3, dtype=float, shape=n * n)
 
-#Implicit method
-F = ti.Vector.field(3, dtype=ti.f32, shape=num_node)
-J = ti.Matrix.field(3, 3, dtype=ti.f32, shape=(num_node, num_node))
-A = ti.Matrix.field(3, 3, dtype=ti.f32, shape=(num_node, num_node))
-M = ti.field(dtype=ti.f32, shape=(num_node, num_node))
-new_v = ti.Vector.field(3, dtype=ti.f32, shape=num_node)
-b = ti.Vector.field(3, dtype=ti.f32, shape=num_node)
-# M.fill(1.0)
-
-# configuration
 bending_springs = False
+F = ti.Vector.field(3, dtype=ti.f32, shape=num_nodes)
+J = ti.Matrix.field(3, 3, dtype=ti.f32, shape=(num_nodes, num_nodes))
+A = ti.Matrix.field(3, 3, dtype=ti.f32, shape=(num_nodes, num_nodes))
+M = ti.field(dtype=ti.f32, shape=(num_nodes, num_nodes))
+new_v = ti.Vector.field(3, dtype=ti.f32, shape=num_nodes)
+b = ti.Vector.field(3, dtype=ti.f32, shape=num_nodes)
+I = ti.Matrix([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+
+
+@ti.func
+def m2v(x):
+    return x[0] * n + x[1]
+
+
+@ti.func
+def v2m(x):
+    return (int(x / n), int(x % n))
 
 
 @ti.kernel
 def initialize_mass_points():
     random_offset = ti.Vector([ti.random() - 0.5, ti.random() - 0.5]) * 0.1
+
     for i, j in x:
         x[i, j] = [
             i * quad_size - 0.5 + random_offset[0], 0.6,
@@ -63,17 +62,18 @@ def initialize_mass_points():
 @ti.kernel
 def initialize_mesh_indices():
     for i, j in ti.ndrange(n - 1, n - 1):
-        quad_id = i * (n - 1) + j
+        quad_id = (i * (n - 1)) + j
+        # 1st triangle of the square
         indices[quad_id * 6 + 0] = i * n + j
         indices[quad_id * 6 + 1] = (i + 1) * n + j
         indices[quad_id * 6 + 2] = i * n + (j + 1)
-
-        indices[quad_id * 6 + 3] = (i + 1) * n + (j + 1)
+        # 2nd triangle of the square
+        indices[quad_id * 6 + 3] = (i + 1) * n + j + 1
         indices[quad_id * 6 + 4] = i * n + (j + 1)
         indices[quad_id * 6 + 5] = (i + 1) * n + j
 
     for i, j in ti.ndrange(n, n):
-        if (i // 4 + j // 4) % 2 == 0:
+        if (i // 2 + j // 2) % 2 == 0:
             colors[i * n + j] = (0.22, 0.72, 0.52)
         else:
             colors[i * n + j] = (1, 0.334, 0.52)
@@ -87,38 +87,12 @@ if bending_springs:
         for j in range(-1, 2):
             if (i, j) != (0, 0):
                 spring_offsets.append(ti.Vector([i, j]))
+
 else:
     for i in range(-2, 3):
         for j in range(-2, 3):
             if (i, j) != (0, 0) and abs(i) + abs(j) <= 2:
                 spring_offsets.append(ti.Vector([i, j]))
-
-
-@ti.kernel
-def collision_detection():
-    for i in ti.grouped(x):
-        v[i] *= ti.exp(-drag_damping * dt)
-        offset_to_center = x[i] - ball_center[0]
-        if offset_to_center.norm() <= ball_radius:
-            normal = offset_to_center.normalized()
-            v[i] -= min(v[i].dot(normal), 0) * normal
-        x[i] += v[i] * dt
-
-
-@ti.func
-def m2v(x):
-    return x[0] * n + x[1]
-
-
-@ti.func
-def v2m(x: int):
-    return (int(x / num_node), int(x % num_node))
-
-
-@ti.kernel
-def init_m():
-    for i in range(num_node):
-        M[i, i] = 1.0
 
 
 @ti.kernel
@@ -133,7 +107,15 @@ def update_force():
                 d = x_ij.normalized()
                 current_dist = x_ij.norm()
                 original_dist = quad_size * float(i - j).norm()
+                # Spring force
                 F[idx] += -spring_Y * d * (current_dist / original_dist - 1)
+
+
+@ti.kernel
+def update_velocity():  #for explicit method
+    for i in ti.grouped(v):
+        idx = m2v(i)
+        v[i] += F[idx] * dt
 
 
 @ti.kernel
@@ -158,20 +140,28 @@ def update_Jacobi():
 
 
 @ti.kernel
-def update_A():
+def update_Ab():
     for i, j in A:
         A[i, j] = M[i, j] * I - dt * dt * J[i, j]
-
-
-@ti.kernel
-def update_b():
     for i in b:
         b[i] = v[v2m(i)] + dt * F[i]
 
 
 @ti.kernel
+def collision_detection():
+    for i in ti.grouped(x):
+        v[i] *= ti.exp(-drag_damping * dt)
+        offset_to_center = x[i] - ball_center[0]
+        if offset_to_center.norm() <= ball_radius:
+            # Velocity projection
+            normal = offset_to_center.normalized()
+            v[i] -= min(v[i].dot(normal), 0) * normal
+        x[i] += dt * v[i]
+
+
+@ti.kernel
 def Jacobi_iteration():
-    n = num_node
+    n = num_nodes
     for i in range(n):
         r = b[i]
         for j in range(n):
@@ -182,26 +172,24 @@ def Jacobi_iteration():
         v[i] = new_v[m2v(i)]
 
 
-def implicit_substep(iter_number):
-    init_m()
-    update_Jacobi()
-    update_A()
+@ti.kernel
+def init_m():
+    for i in range(num_nodes):
+        M[i, i] = 1.0
+
+
+def substep():
     update_force()
-    update_b()
-    for _ in range(iter_number):
-        Jacobi_iteration()
+    update_velocity()
     collision_detection()
 
 
-@ti.kernel
-def update_velocity():
-    for i in ti.grouped(v):
-        v[i] += F[m2v(i)] * dt
-
-
-def explicit_substep():
+def implicit_substep(num_iteration):
     update_force()
-    update_velocity()
+    update_Jacobi()
+    update_Ab()
+    for _ in range(num_iteration):
+        Jacobi_iteration()
     collision_detection()
 
 
@@ -211,7 +199,8 @@ def update_vertices():
         vertices[i * n + j] = x[i, j]
 
 
-window = ti.ui.Window("Cloth Simulation", (1024, 1024), vsync=True)
+window = ti.ui.Window("Taichi Cloth Simulation on GGUI", (1024, 1024),
+                      vsync=True)
 canvas = window.get_canvas()
 canvas.set_background_color((1, 1, 1))
 scene = ti.ui.Scene()
@@ -220,23 +209,18 @@ camera = ti.ui.Camera()
 current_t = 0.0
 initialize_mass_points()
 
-# dump to images
-# result_dir = "../video"
-# video_manager = ti.tools.VideoManager(output_dir=result_dir,
-#   framerate=24,
-#   automatic_build=True)
-
-#rendering loop
 while window.running:
-    if current_t > 30:
+    init_m()
+    if current_t > 100:
+        # Reset
         initialize_mass_points()
         current_t = 0
 
     for i in range(substeps):
         if use_implicit:
-            implicit_substep(iter_number)
+            implicit_substep(num_iteration)
         else:
-            explicit_substep()
+            substep()
         current_t += dt
     update_vertices()
 
@@ -251,9 +235,9 @@ while window.running:
                per_vertex_color=colors,
                two_sided=True)
 
+    # Draw a smaller ball to avoid visual penetration
     scene.particles(ball_center,
                     radius=ball_radius * 0.8,
                     color=(0.5, 0.42, 0.8))
     canvas.scene(scene)
-    # video_manager.write_frame(window.get_image_buffer_as_numpy())
     window.show()
